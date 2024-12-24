c8955673c14f44d641ea344d48a5c5e771721ba0
Yahor <151521406+ThomasWhite212@users.noreply.github.com>
2024-12-24 11:08:52 +0300

    Create #3_Task.java

---
+++ #3_Task.java
@@ -0,0 +1,19 @@
+// 3. Создайте метод printColor() в теле которого задайте int переменную value и инициализируйте ее любым значением. Если value меньше 0 (0 включительно), то в консоль метод должен вывести сообщение "Красный", если лежит в пределах от 0 (0 исключительно) до 100 (100 включительно), то "Желтый", если больше 100 (100 исключительно) - "Зеленый".
+
+public class Task_3 {
+    public static void main(String[] args) {
+        printColor();
+    }
+
+    public static void printColor() {
+        int value = -10; // Можно задать значение
+
+        if (value <= 0) {
+            System.out.println("Красный");
+        } else if (value > 0 && value <= 100) {
+            System.out.println("Желтый");
+        } else {
+            System.out.println("Зеленый");
+        }
+    }
+}
